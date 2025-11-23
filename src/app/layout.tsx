8404d3414faa057b0c7f46486da88b8fa4f0bf88
Paweł Chyła <chyla1988@gmail.com>
2025-11-23 02:34:10 +0100

    Use header componenet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,8 @@
 import "./globals.css";
-import { LucideKanban } from "lucide-react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link";
+import { Header } from "@/components/header";
 import { ThemeProvider } from "@/components/theme/theme-provider";
-import { ThemeSwitcher } from "@/components/theme/theme-switcher";
-import { buttonVariants } from "@/components/ui/button"
-import { homePath, ticketsPath } from "@/paths";
 
 
 
@@ -35,33 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider>
-          <nav
-            className="
-            supports-backdrop-blur:bg-background/60
-            fixed left-0 right-0 top-0 z-20
-            border-b bg-background/95 backdrop-blur
-            w-full flex py-2.5 px-5 justify-between
-          "
-          >
-            <div className="flex align-items gap-x-2">
-              <Link
-                href={homePath()}
-                className={buttonVariants({ variant: "ghost" })}
-              >
-                <LucideKanban />
-                <h1 className="text-lg font-semibold">TicketBounty</h1>
-              </Link>
-            </div>
-            <div className="flex align-items gap-x-2">
-              <ThemeSwitcher />
-              <Link
-                href={ticketsPath()}
-                className={buttonVariants({ variant: "outline" })}
-              >
-                Tickets
-              </Link>
-            </div>
-          </nav>
+          <Header />
           <main
             className="
             min-h-screen flex-1
