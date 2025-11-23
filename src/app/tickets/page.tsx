0a3cc160ce107ce1efd6b318ea41d25dd056a985
Paweł Chyła <chyla1988@gmail.com>
2025-11-23 02:34:01 +0100

    Fetch tickets on client

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,15 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Heading } from "@/components/heading";
-import { initialTickets } from "@/data";
 import { TicketItem } from "@/modules/tickets/componenets/ticket-item";
+import { getTickets } from "@/modules/tickets/queries/getTickets";
+import { Ticket } from "@/modules/tickets/types";
 
 
 const TicketsPage = () => {
+    const [tickets, setTickets] = useState<Ticket[]>([])
+
+    useEffect(() => {
+        const fetchTickets = async () => {
+            const res = await getTickets();
+            setTickets(res)
+        }
+
+        fetchTickets();
+    }, [])
+
+
     return (
         <div className="flex-1 flex flex-col gap-y-8">
             <Heading title="Tickets" description="All your tickets at one place" />
 
             <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-                {initialTickets.map((ticket) => (
+                {tickets.map((ticket) => (
                     <TicketItem key={ticket.id} ticket={ticket} />
                 ))}
             </div>
---
+++ src/modules/tickets/queries/getTickets.ts
@@ -0,0 +1,6 @@
+import { initialTickets } from "@/data"
+
+export const getTickets = async () => {
+    await new Promise(res => setTimeout(res, 2000))
+    return initialTickets
+}
