b825ecf024de75e8b6cd1635c9fa125ac6bd17cb
Paweł Chyła <chyla1988@gmail.com>
2025-11-24 01:29:10 +0100

    Seeding database

---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+import { prisma } from "@/lib/prisma"
+
+export const tickets = [
+    {
+        title: "Ticket 1",
+        content: "This is the first ticket from database.",
+        status: "DONE" as const,
+    },
+    {
+        title: "Ticket 2",
+        content: "This is the second ticket from database.",
+        status: "OPEN" as const,
+    },
+    {
+        title: "Ticket 4",
+        content: "This is the thirs ticket from database.",
+        status: "IN_PROGRESS" as const,
+    },
+];
+
+const seed = async () => {
+    console.log("Starting seeding...")
+    const t1 = performance.now();
+
+    await prisma.ticket.deleteMany();
+    await prisma.ticket.createMany({
+        data: tickets
+    })
+
+    const t2 = performance.now();
+    console.log(`Finished seeding in ${t2 - t1} ms`)
+}
+
+seed()
